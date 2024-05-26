f4c1d0cfc521566b4a1b4a8203ac66d0bb620628
Jose Bermudez <joseber310@gmail.com>
2024-05-26 13:21:27 -0500

    api key and host as env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'e10449344c3ed4d23dbc39c2bf657aa8&hash=ac9cd13f55ac10371645a0126b7f009c&ts=abcdefghijk',
+  marvelHost: 'https://gateway.marvel.com'
 };
